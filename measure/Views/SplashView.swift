968a20e9c72dbe2593a2e91a49ce8d88bd856696
Edward Focer <edwardfocer704@gmail.com>
2025-05-14 22:42:49 +0530

    Added Splash Screen

---
+++ measure/Views/SplashView.swift
@@ -0,0 +1,45 @@
+//
+//  SplashView.swift
+//  measure
+//
+//  Created by Apnitor Solutions on 14/05/25.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    @State private var isActive = false
+
+    var body: some View {
+        if isActive {
+            ContentView()
+        } else {
+            ZStack {
+                LinearGradient(
+                    gradient: Gradient(colors: [
+                        Color(red: 0/255, green: 46/255, blue: 94/255),
+                        Color(red: 51/255, green: 192/255, blue: 224/255)
+                    ]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                .ignoresSafeArea()
+
+                VStack {
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 210, height: 105)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation {
+                        isActive = true
+                    }
+                }
+            }
+        }
+    }
+}
--- measure/measureApp.swift
+++ measure/measureApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct measureApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashView()
         }
     }
 }
