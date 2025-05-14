1cc74d3dc858070a98642181e8bb87655cb7913b
Edward Focer <edwardfocer704@gmail.com>
2025-05-14 22:20:51 +0530

    Initial commit

---
+++ measure/measureApp.swift
@@ -0,0 +1,17 @@
+//
+//  measureApp.swift
+//  measure
+//
+//  Created by Apnitor Solutions on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct measureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
